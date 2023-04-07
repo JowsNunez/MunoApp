756ab7fd5dc10e2adf83f8117ae0736065bc8b89
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-04-06 22:25:51 -0700

    se creo la pantalla portafolio

---
+++ app/src/main/java/com/proyecto/munoapp/PortafolioActivity.kt
@@ -0,0 +1,11 @@
+package com.proyecto.munoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PortafolioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portafolio)
+    }
+}
