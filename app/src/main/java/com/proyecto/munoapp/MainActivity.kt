7a5a459a424d440c802a9aeb3c6cdb0694828d5e
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-04-09 13:37:50 -0700

    se agrego pantalla de configuracion

--- app/src/main/java/com/proyecto/munoapp/MainActivity.kt
+++ app/src/main/java/com/proyecto/munoapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.proyecto.munoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -30,6 +32,18 @@ class MainActivity : AppCompatActivity() {
                R.id.navigation_tareas,   R.id.navigation_proyecto,R.id.navigation_mensaje, R.id.navigation_notificacion, R.id.navigation_almacenamiento
             )
         )
+
+        val buttonConfig = binding.btnConfig
+        val img = binding.imgUser
+
+        buttonConfig.setOnClickListener{
+            val intent: Intent = Intent(this, ConfigActivity::class.java)
+            startActivity(intent)
+        }
+
+        img.setOnClickListener {
+            Toast.makeText(this,"TODO:: abrir perfil",Toast.LENGTH_SHORT).show()
+        }
 
         navView.setupWithNavController(navController)
     }
