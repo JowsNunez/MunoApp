5bee84da652c1f2e0420176015a5a6ba9aacce8d
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-04-16 17:37:07 -0700

    se agregaron activity de proyecto en blanco

---
+++ app/src/main/java/com/proyecto/munoapp/SalaActivity.kt
@@ -0,0 +1,11 @@
+package com.proyecto.munoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SalaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sala)
+    }
+}
